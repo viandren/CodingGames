057379523dc37c99770188e520485a169d691138
Viandren <viandren.home@gmail.com>
2020-03-28 14:12:43 +0100

    init

---
+++ CodingGames/src/main/java/com/mycompany/codinggames/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.codinggames;
+
+import com.mycompany.codinggames.euler.problems.p30_39.p31.Problem34;
+
+/**
+ *
+ * @author viand
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Problem31 p31 = new Problem31();
+        //System.out.println(p31.getAnswer());
+        //Problem32 p32 = new Problem32();
+        //System.out.println(p32.getAnswer());
+        //Problem33 p33 = new Problem33();
+        //System.out.println(p33.getAnswer());
+        Problem34 p34 = new Problem34();
+        System.out.println(p34.getAnswer());
+        
+    }
+    
+}
