bfd423d9a1af8b97a4aa165eba2c155299123835
RobertFrank <robert.frank@frontsoft.hu>
2021-01-18 11:28:07 +0100

    a rekurzív bejárás még mindig túl lassú

--- Coding_Games/src/main/java/hu/viandren/coding_games/euler/F11_F20/F15.java
+++ Coding_Games/src/main/java/hu/viandren/coding_games/euler/F11_F20/F15.java
@@ -18,35 +18,21 @@ public class F15 {
   }
 
 
-  public static boolean isleaplegal(int m, int x1, int y1, int x2, int y2) {
-    if (x1 <= m && y1 <= m) {
-      return (((x1 == x2) && (y1 + 1) == y2) || ((y1 == y2) && (x1 + 1) == x2));
-    } else {
-      return false;
-    }
-  }
-
   public static long leapcount(int m, int x1, int y1) {
 
-    long cnt = 0;
-    int x2, y2;
 
-    if (x1 == m && y1 == m) {
-      cnt = 1;
+    if (x1 == y1 && y1 == m) {
+      return 1;
     } else {
-
-      x2 = x1 + 1;
-      y2 = y1;
-      if (isleaplegal(m, x1, y1, x2, y2)) {
-        cnt += leapcount(m, x2, y2);
+      long cnt = 0;
+      if (x1 < m) {
+        cnt += leapcount(m, x1 + 1, y1);
       }
-      x2 = x1;
-      y2 = y1 + 1;
-      if (isleaplegal(m, x1, y1, x2, y2)) {
-        cnt += leapcount(m, x2, y2);
+      if (y1 < m) {
+        cnt += leapcount(m, x1, y1 + 1);
       }
+      return cnt;
     }
-    return cnt;
 
   }
 }
